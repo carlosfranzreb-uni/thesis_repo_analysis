e76f3a28b8345a4697543c9eab2f8910ec16a97e
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-04-07 12:53:39 +0200

    Add class to harvest publications.

---
+++ harvester.py
@@ -0,0 +1,54 @@
+""" Harvests publications from a OAI-PMH repository. """
+
+import xml.etree.ElementTree as ET
+import os
+import requests as req
+
+
+class Harvester:
+  def __init__(self, base_url, folder):
+    self.base_url = base_url
+    self.folder = folder
+    if not os.path.isdir(folder):
+      raise OSError('Folder does not exist.')
+    self.metadata_prefix = 'oai_dc'
+    self.oai = '{http://www.openarchives.org/OAI/2.0/}'
+
+  def request(self, verb, params=dict()):
+    """ Executes a request with the given verbs and parameters. The parameters
+    are given as a dict. """
+    request = f'{self.base_url}?verb={verb}'
+    for key, value in params.items():
+      request += f'&{key}={value}'
+    if 'resumptionToken' not in params:
+      request += f'&metadataPrefix={self.metadata_prefix}'
+    return req.get(request)
+
+  def retrieve_all(self):
+    """ Retrieve all publications. """
+    done = False
+    cnt = 1
+    res = self.request('ListRecords')
+    while not done:
+      with open(f'data/publications_{cnt}.xml', 'w', encoding='utf8') as f:
+        f.write(res.text)
+      cnt += 1
+      token = self.get_resumption_token(res.text)
+      if len(token) == 0:
+        done = True
+      else:
+        res = self.request('ListRecords', {'resumptionToken': token})
+  
+  def get_resumption_token(self, text):
+    return ET.fromstring(text) \
+      .find(f'{self.oai}ListRecords') \
+      .find(f'{self.oai}resumptionToken') \
+      .text
+
+
+if __name__ == "__main__":
+  # harvester = Harvester(
+  #   'https://depositonce.tu-berlin.de/oai/request',
+  #   'data/depositonce'
+  # )
+  # harvester.retrieve_all()
