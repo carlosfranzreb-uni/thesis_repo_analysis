82e7877847cbecc4d1bc17dd19b100431385382f
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-04-20 10:47:57 +0200

    Create file that retrieves any field from the XML.

--- retrieve_fields.py
+++ retrieve_fields.py
@@ -1,4 +1,4 @@
-"""Retrieve the authors of the publications and create a dict with authors
+"""Retrieve any field of the publications and create a dict with them
 as keys and their publications as a list of values. """
 
 import os
@@ -11,10 +11,10 @@ oai_dc = '{http://www.openarchives.org/OAI/2.0/oai_dc/}'
 dc = '{http://purl.org/dc/elements/1.1/}'
 
 
-def retrieve_authors(folder):
-  """ Extract the creators of each publication and store them as a
+def retrieve_fields(folder, dump, field):
+  """ Extract the fields of each publication and store them as a
   list. """
-  creators = dict()
+  fields = dict()
   for f in os.listdir(folder):
     root = ET.parse(f'{folder}/{f}').getroot()
     records = root.find(f'{oai}ListRecords')
@@ -27,19 +27,19 @@ def retrieve_authors(folder):
       try:
         id = header.find(f'{oai}identifier').text
         metadata = record.find(f'{oai}metadata').find(f'{oai_dc}dc')
-        for creator in metadata.findall(f'{dc}creator'):
-          if creator.text not in creators:
-            creators[creator.text] = list()
-          creators[creator.text].append(id)
+        for fi in metadata.findall(field):
+          if fi.text not in fields:
+            fields[fi.text] = list()
+          fields[fi.text].append(id)
       except AttributeError:
         print(f, id)
         import sys; sys.exit(0)
-  json.dump(creators, open('data/json/depositonce/authors.json', 'w'))
+  json.dump(fields, open(dump, 'w'))
 
 
-def retrieve_authors_reversed(folder):
-  """ Same as retrieve_authors(), but now the publications are the keys
-  and the authors the values. """
+def retrieve_fields_reversed(folder, dump, field):
+  """ Same as retrieve_fields(), but now the publications are the keys
+  and the fields the values. """
   publications = dict()
   for f in os.listdir(folder):
     root = ET.parse(f'{folder}/{f}').getroot()
@@ -53,12 +53,16 @@ def retrieve_authors_reversed(folder):
       try:
         id = header.find(f'{oai}identifier').text
         metadata = record.find(f'{oai}metadata').find(f'{oai_dc}dc')
-        publications[id] = [c.text for c in metadata.findall(f'{dc}creator')]
+        publications[id] = [c.text for c in metadata.findall(field)]
       except AttributeError:
         print(f, id)
         import sys; sys.exit(0)
-  json.dump(publications, open('data/json/refubium/authors_reversed.json', 'w'))
+  json.dump(publications, open(dump, 'w'))
 
 
 if __name__ == "__main__":
-  retrieve_authors_reversed('data/xml/refubium')
+  dump = 'data/json/refubium/contributors.json'
+  dump_r = 'data/json/refubium/contributors_reversed.json'
+  field = f'{dc}contributor'
+  retrieve_fields('data/xml/refubium', dump, field)
+  retrieve_fields_reversed('data/xml/refubium', dump_r, field)
