a651d4ed2e0068d18f1ec7c2465fbb64b98bae6c
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-04-28 10:12:13 +0200

    Don't retrieve description; retrieve language.
    Full texts will be used as data.

--- process_data.py
+++ process_data.py
@@ -1,17 +1,12 @@
 """ Process the XML files retrieved with the Harvester class (harvester.py).
-- Extract the relevant fields: title, description, date, authors,
-contributors, subjects, publication type and language.
-- Discard anything written in english.
-- If an item has multiple english descriptions, keep only the longest one.
-- remove uninformative descriptions.
-- process descriptions with the functions of retrieve_descriptions.py """
+- Extract the relevant fields: title, date, authors, contributors, subjects,
+publication type and language.
+- If an item has two titles, keep only the english one. """
 
 
 import os
-from retrieve_descriptions import preprocess
 import xml.etree.ElementTree as ET
 import json
-from nltk.stem import WordNetLemmatizer
 import langdetect
 
 oai = '{http://www.openarchives.org/OAI/2.0/}'
@@ -25,24 +20,26 @@ class Processor:
     self.folder = folder
     self.data = list()
     self.fields = [
+      f'{dc}title',
       f'{dc}creator',
       f'{dc}contributor',
       f'{dc}date',
       f'{dc}subject',
       f'{dc}type',
-      f'{dc}description',
-      f'{dc}subject',
+      f'{dc}language',
     ]
-    self.wnl = WordNetLemmatizer()
+    self.types = json.load(open('analysis/types/clustered_types.json'))
+    self.dump_file = dump_file
   
   def process(self):
     self.retrieve_objects()
-    self.dump(dump_file)
+    self.dump()
 
   def retrieve_objects(self):
     """ Extract the objects from the XML files that reside
     in the given folder. """
     for f in os.listdir(self.folder):
+      print(f)
       root = ET.parse(f'{self.folder}/{f}').getroot()
       records = root.find(f'{oai}ListRecords')
       for record in records:
@@ -64,30 +61,54 @@ class Processor:
     for field in self.fields:
       data = []
       for fi in metadata.findall(field):
-        if len(fi.text) == 0:
+        if fi.text is None or len(fi.text) == 0:
           continue
-        try:
-          if langdetect.detect(fi.text) == 'en' and fi.text not in data:
-            data.append(fi.text)
-        except langdetect.lang_detect_exception.LangDetectException:
-          if fi.text not in data:
-            data.append(fi.text)
+        elif fi.text not in data:
+          data.append(fi.text) 
       if len(data) == 0:
         data = None
-      elif 'description' in field:
-        data = preprocess(max(data, key=len), self.wnl)
+      elif 'type' in field:
+        data = self.prune_types(data)
+      elif 'language' in field and len(data) > 1:
+        data = min(data, key=len)
+      elif 'title' in field:
+        if len(data) > 1:
+          english_data = []
+          for t in data:
+            try:
+              if langdetect.detect(t) == 'en':
+                english_data.append(t)
+            except langdetect.lang_detect_exception.LangDetectException:
+              english_data.append(t)
+          if len(english_data) == 0:
+            data = data[0]
+          else:
+            data = max(english_data, key=len)
+        else:
+          data = data[0]
       elif len(data) == 1:
         data = data[0]
       object[field.replace(dc, '')] = data
     self.data.append(object)
 
-  def dump(self, dump_file):
-    json.dump(self.data, open(dump_file, 'w'))
+  def dump(self):
+    json.dump(self.data, open(self.dump_file, 'w'))
+
+  def prune_types(self, data):
+    """Extract the correct types. """
+    for d in data:
+      d = d.lower().replace('doc-type:', '').replace(' ', '')
+      if 'version' in data:
+        continue
+      for category in self.types:
+        for t in self.types[category]:
+          if d == t.lower().replace(' ', ''):
+            return category
 
 
 if __name__ == "__main__":
-  folder = 'data/xml/depositonce'
-  dump_file = 'data/processed/depositonce.json'
-  processor = Processor(folder, dump_file)
-  processor.process()
+  folders = ['depositonce', 'edoc', 'refubium']
+  for folder in folders:
+    processor = Processor(f'data/xml/{folder}', f'data/processed/{folder}.json')
+    processor.process()
         
