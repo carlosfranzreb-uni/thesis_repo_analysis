9862b45d56a7fb5ad31eee856381dae5e7ec685c
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-05-13 18:56:09 +0200

    Catch decoding errors.

--- retrieval/didl/retrieve_texts.py
+++ retrieval/didl/retrieve_texts.py
@@ -87,7 +87,11 @@ class Harvester:
       if lang in ('en', 'eng') and self.correct_type(id):
         link = metadata.find(f'{self.didl}Component') \
           .find(f'{self.didl}Resource').attrib['ref']
-        res = req.get(link)
+        try:
+          res = req.get(link)
+        except UnicodeDecodeError:
+          logging.error(f'{filename} couldn\'t be decoded')
+          continue
         filename = self.pdf_folder.split('/')[-1] + '_' + id.split('/')[-1]
         if f'{filename}.txt' in self.existing_txt:
           logging.info(f'File {filename} has already been retrieved.')
