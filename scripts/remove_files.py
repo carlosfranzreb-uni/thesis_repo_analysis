c8b25f5965ae7d9e28fa409d9a214760bbfd1462
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-05-18 13:15:39 +0200

    Some filenames include points.

--- scripts/remove_files.py
+++ scripts/remove_files.py
@@ -5,7 +5,7 @@ import os
 import re
 
 
-regexp = re.compile('depositonce_[0-9]+_[0-9]+')
+regexp = re.compile('depositonce_[0-9.]+_[0-9]+')
 folder = '../data/texts/txt/depositonce'
 for f in os.listdir(folder):
   if regexp.match(f):
