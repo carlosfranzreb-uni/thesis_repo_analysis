474b497caaa8c98d9aec95f49da46e5b78159635
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-05-18 12:52:51 +0200

    Script to remove duplicates.

---
+++ remove_files.py
@@ -0,0 +1,12 @@
+""" Remove duplicates from the txt folders. """
+
+
+import os
+import re
+
+
+regexp = re.compile('depositonce_[0-9]+_[0-9]+')
+folder = 'data/texts/txt/depositonce'
+for f in os.listdir(folder):
+  if regexp.match(f):
+    os.remove(f'{folder}/{f}')
