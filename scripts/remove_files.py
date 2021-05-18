6844ef572e0bbb5c0434d551913367a00c8463b8
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-05-18 13:01:44 +0200

    Move file.

--- scripts/remove_files.py
+++ scripts/remove_files.py
@@ -6,7 +6,7 @@ import re
 
 
 regexp = re.compile('depositonce_[0-9]+_[0-9]+')
-folder = 'data/texts/txt/depositonce'
+folder = '../data/texts/txt/depositonce'
 for f in os.listdir(folder):
   if regexp.match(f):
     os.remove(f'{folder}/{f}')
