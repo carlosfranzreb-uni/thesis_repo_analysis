6714d1f8b88dbd33aebdacf788549c22c543b368
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-06-02 11:20:05 +0200

    Add script to find missing files.

--- scripts/missing_files.py
+++ scripts/missing_files.py
@@ -3,15 +3,56 @@ relevant documents are missing. """
 
 
 import json
+import os
 
-repo = 'depositonce'
-files = json.load(open(f'../data/texts/txt/{repo}_files.json'))
-items = json.load(open(f'../data/json/dim/{repo}/relevant_ids.json'))
-ids = set([item.split('/')[1] for item in items])
-file_ids = set([f[:-4].split('_')[1] for f in files])
-assert len(ids) == len(items)
-assert len(file_ids) == len(files)
-missing = ids - file_ids
-print(len(missing))
-print(len(file_ids - ids))
-print(len(ids) - len(file_ids))
+
+def prinf_facts():
+  """ Print some facts about the missing files. """
+  for repo in ('depositonce', 'edoc', 'refubium'):
+    files = json.load(open(f'../data/texts/txt/{repo}_files.json'))
+    items = json.load(open(f'../data/json/dim/{repo}/relevant_ids.json'))
+    ids = set([item.split('/')[1] for item in items])
+    file_ids = set([f[:-4].split('_')[1] for f in files])
+    assert len(ids) == len(items)
+    assert len(file_ids) == len(files)
+    missing = ids - file_ids
+    print(f'{repo} has {len(files)} files.')
+    print(len(missing))
+    print(len(file_ids - ids))
+    print(len(ids) - len(file_ids))
+
+
+def search_logs():
+  """ Search the logs for the missing files. """
+  missing_logs = {}
+  for repo in ('depositonce', 'edoc', 'refubium'):
+    files = json.load(open(f'../data/texts/txt/{repo}_files.json'))
+    items = json.load(open(f'../data/json/dim/{repo}/relevant_ids.json'))
+    ids = set([item.split('/')[1] for item in items])
+    file_ids = set([f[:-4].split('_')[1] for f in files])
+    missing = ids - file_ids
+    missing_files = [f'{repo}_{id}' for id in missing]
+    for missing_file in missing_files:
+      for filename in os.listdir('../logs'):
+        with open(f'../logs/{filename}') as f:
+          if repo not in f.readline():
+            continue
+          for line in f:
+            if missing_file + " " in line:
+              missing_logs[missing_file] = line
+  json.dump(missing_logs, open('missing_files.json', 'w'))
+
+
+def get_missing_ids(repo):
+  files = json.load(open(f'../data/texts/txt/{repo}_files.json'))
+  items = json.load(open(f'../data/json/dim/{repo}/relevant_ids.json'))
+  ids = set([item.split('/')[1] for item in items])
+  file_ids = set([f[:-4].split('_')[1] for f in files])
+  return ids - file_ids 
+
+
+if __name__ == "__main__":
+  for repo in ('depositonce', 'edoc', 'refubium'):
+    missing = get_missing_ids(repo)
+    json.dump(list(missing), open(f'{repo}_missing.json', 'w'))
+
